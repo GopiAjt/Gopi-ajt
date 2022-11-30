b6c14efd054bbbf9d8e35acb49734793dbb602aa
GopiAjt <116329111+GopiAjt@users.noreply.github.com>
2022-11-30 09:04:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+//objects
+let person = {
+    name:"gopi",
+    age:"22",
+    yop:"2022",
+    city:"belagaum",
+    stream:"cs"
+}
+console.log(person.name);
+console.log(person.city);
+person.city="belngaluru";
+console.log(person.city);
+
+//strings
+let me = "gopi"
+
+console.log(me.toLocaleUpperCase());
+console.log(me.toLocaleLowerCase());
+
+let is = "  trish  "
+console.log(is.trim());
+
+console.log(me.charAt(2));
+
+var a = 10
+
+console.log(`value of a is ${d}`);//templet string using back tick key
+console.log("value of a is "+a);
